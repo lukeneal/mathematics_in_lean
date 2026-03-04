7a4a4b5d6a8ea0bf9be0bb2f32aa9a72a19c851c
lukeneal <lukeneal9@gmail.com>
2026-03-03 23:41:37 -0500

    Finished S03

--- MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -44,7 +44,10 @@ example (x : ℝ) : x ≤ x :=
 
 -- Try this.
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
-  sorry
+  apply lt_of_le_of_lt h₀
+  apply lt_trans h₁
+  apply lt_of_le_of_lt h₂
+  exact h₃
 
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
   linarith
@@ -75,6 +78,7 @@ example (h : 1 ≤ a) (h' : b ≤ c) : 2 + a + exp b ≤ 3 * a + exp c := by
 #check (add_pos_of_pos_of_nonneg : 0 < a → 0 ≤ b → 0 < a + b)
 #check (exp_pos : ∀ a, 0 < exp a)
 #check add_le_add_left
+-- I wonder if these are axioms or had to be proven using more fundamental axioms. You can check the source code to find out.
 
 example (h : a ≤ b) : exp a ≤ exp b := by
   rw [exp_le_exp]
@@ -86,21 +90,34 @@ example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
     apply exp_lt_exp.mpr h₁
   apply le_refl
 
-example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by sorry
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  apply add_le_add_left
+  apply exp_le_exp.mpr
+  apply add_le_add_left
+  exact h₀
 
 example : (0 : ℝ) < 1 := by norm_num
 
 example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
-  have h₀ : 0 < 1 + exp a := by sorry
+  have h₀ : 0 < 1 + exp a := by
+    apply add_pos
+    norm_num
+    apply exp_pos
   apply log_le_log h₀
-  sorry
+  apply add_le_add_left
+  apply exp_le_exp.mpr
+  exact h
 
 example : 0 ≤ a ^ 2 := by
   -- apply?
   exact sq_nonneg a
 
 example (h : a ≤ b) : c - exp b ≤ c - exp a := by
-  sorry
+  -- linarith [exp_le_exp.mpr h] -- AI suggestion. Alternative solution
+  refine tsub_le_tsub ?_ ?_
+  apply le_refl
+  apply exp_le_exp.mpr
+  exact h
 
 example : 2*a*b ≤ a^2 + b^2 := by
   have h : 0 ≤ a^2 - 2*a*b + b^2
@@ -121,7 +138,29 @@ example : 2*a*b ≤ a^2 + b^2 := by
   linarith
 
 example : |a*b| ≤ (a^2 + b^2)/2 := by
-  sorry
+  have h₀ : (a^2 + b^2)/2 = (a-b)^2/2 + (a*b) := by ring
+  have h₁ : 0 ≤ (a-b)^2/2 := by
+    apply div_nonneg
+    apply pow_two_nonneg
+    norm_num
+  apply abs_le'.mpr
+  constructor
+  rw[h₀]
+  calc
+    a*b = 0 + a*b := by ring
+    _ ≤ (a-b)^2/2 + a*b := by apply add_le_add_right h₁
+  have h₂ : (a^2 + b^2)/2 + (a*b) = (a+b)^2/2 := by ring
+  have h₃ : 0 ≤ (a^2 + b^2)/2 + (a*b) := by
+    rw[h₂]
+    apply div_nonneg
+    apply pow_two_nonneg
+    norm_num
+  have h₄ : 0 + -(a*b) ≤ (a^2 + b^2)/2 + (a*b) - (a*b) := by
+      apply add_le_add_right
+      exact h₃
+  rw [zero_add] at h₄
+  have h₅ : (a^2 + b^2)/2 + (a*b) - (a*b) = (a^2 + b^2)/2 := by ring
+  rw[h₅] at h₄
+  exact h₄
 
 #check abs_le'.mpr
-
